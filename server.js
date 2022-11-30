c9abfed2f69f7798b74d73cd1f611e49eae0e7f9
Sharon Ogbonna <114447309+Sharonogbonna@users.noreply.github.com>
2022-11-30 10:53:32 -0600

    Tip Calculator express application created

--- server.js
+++ server.js
@@ -2,11 +2,18 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+//GREETING
 app.get('/greeting', (req, res)=>{
     res.send('Hey, queen! Girl, you have done it again.')
 })
 app.get('/greeting/:name', (req, res) => {
     res.send(req.params.name.charAt(0).toUpperCase()+ req.params.name.slice(1) + ', you are constantly raising the bar for all of us and doing it flawlessly.')
+})
+
+//TIP CALCULATOR
+app.get('/tip/:total/:tipPercentage', (req, res) => {
+    let tipAmount = req.params.total * (req.params.tipPercentage/100) 
+    res.send('Your calculated tip should be $' + tipAmount)
 })
 
 app.listen(port, function () {
