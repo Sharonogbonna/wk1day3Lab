1a4c6e9e83deb8aedb2fbbd5c8d091fc75ff4760
Sharon Ogbonna <114447309+Sharonogbonna@users.noreply.github.com>
2022-11-30 11:21:47 -0600

    Magic 8 Ball express application created

--- server.js
+++ server.js
@@ -13,9 +13,16 @@ app.get('/greeting/:name', (req, res) => {
 //TIP CALCULATOR
 app.get('/tip/:total/:tipPercentage', (req, res) => {
     let tipAmount = req.params.total * (req.params.tipPercentage/100) 
-    res.send('Your calculated tip should be $' + tipAmount)
+    res.send(`Your calculated tip is $${tipAmount}`)
 })
-
+//MAGIC 8 BALL
+app.get('/magic/:phrase', (req, res) => {
+    let responses = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
+    randomIndex = Math.ceil(Math.random()*(responses.length-1)) 
+    console.log(responses.length)
+    console.log(randomIndex)
+    res.send(`You asked: ${req.params.phrase}? <br></br> <h1>${responses[randomIndex]}</h1>`)
+})
 app.listen(port, function () {
     console.log('Listening...');
   });
